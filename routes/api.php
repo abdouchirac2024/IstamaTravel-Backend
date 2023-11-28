96102b38d87e9b6c91454721163f1f3389e0504c
abdouchirac2024 <abdouchirac411@gmail.com>
2023-11-28 15:56:41 +0100

    Votre message de commit

---
+++ routes/api.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RoleController;
+use App\Http\Controllers\Api\TrajetController;
+use App\Http\Controllers\Api\ChauffeurController;
+use App\Http\Controllers\Api\BusController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Route pour obtenir les informations de l'utilisateur actuellement authentifié
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+   return $request->user();
+});
+
+// Routes du contrôleur AuthController avec le préfixe 'auth'
+Route::controller(App\Http\Controllers\Api\Auth\AuthController::class)->prefix('auth')->group(function () {
+   Route::post('/login', 'login');
+   Route::post('/register', 'register');
+   Route::post('/logout', 'logout')->middleware('auth:sanctum');
+});
+
+// Routes du contrôleur AgentController avec le middleware auth:sanctum
+Route::middleware(['auth:sanctum'])->group(function () {
+   Route::apiResource('agents', App\Http\Controllers\Api\AgentController::class);
+   Route::apiResource('roles', RoleController::class); // Routes du contrôleur RoleController avec le middleware auth:sanctum
+  
+  
+});
+
+// Routes du contrôleur StudentController
+Route::apiResource('students', App\Http\Controllers\Api\StudentController::class);
+
+Route::post('trajets/search', [TrajetController::class, 'search']);
+
+Route::apiResource('trajets', TrajetController::class);
+Route::resource('chauffeurs', ChauffeurController::class);
+Route::resource('buses', BusController::class);
+Route::get('buses/trashed', [BusController::class, 'indexTrashed']);
+Route::get('buses/not-trashed', [BusController::class, 'indexNotTrashed']);
